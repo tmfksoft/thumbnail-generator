55db2df3c854b14cd6bcdd4f26184322024418e6
Luke Thompson <luke@streamjar.tv>
2019-08-31 21:20:14 +0100

    Bundle typescript correctly

--- example/sample.ts
+++ example/sample.ts
@@ -1,4 +1,4 @@
-import { generateSprites } from '../index';
+import { generateSprites } from '../src';
 import * as fs from 'fs';
 
 console.log(generateSprites);
@@ -6,11 +6,11 @@ console.log(generateSprites);
 // generateSprites is a promise. So we can 'spray and pray' and like a boomerang it'll come back to us when its done.
 generateSprites({
     source: '../input/bbb_sunflower_1080p_30fps_normal.mp4'
-}).then( out => {
+}).then(out => {
     console.log(`I generated ${out.pageCount} pages out from ${out.frameCount} frames.`);
     for (let page of out.pages) {
         fs.writeFileSync(`example_${out.pages.indexOf(page)}.png`, page);
     }
-}).catch( err => {
+}).catch(err => {
     console.log(err);
 });
